3033cd8874fd7a8cc3cdc09a35341f7a59cb64d5
Kulakov619 <kulakov_i_v@school619.ru>
2024-10-04 09:46:24 +0300

    Init 4

--- core/test_app/admin.py
+++ core/test_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TestModel
 
-# Register your models here.
+
+@admin.register(TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
--- core/test_app/models.py
+++ core/test_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TestModel(models.Model):
+    id = models.IntegerField(
+        'id', unique=True, primary_key=True)
+    name = models.CharField('name', max_length=20)
+    text = models.TextField('text')
+    img = models.ImageField('image', upload_to='images/')
+
+    class Meta:
+        verbose_name = "post"
+        verbose_name_plural = "posts"
+
+    def __str__(self):
+        return self.name
+
