103425a8c02a2539a265958a323bed9f123841f7
Kulakov619 <kulakov_i_v@school619.ru>
2024-10-04 11:01:47 +0300

    Init 4

--- core/test_app/views.py
+++ core/test_app/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from .models import TestModel
 
 
 class TestPage(TemplateView):
@@ -7,4 +8,10 @@ class TestPage(TemplateView):
 
 class TestPageNew(TemplateView):
     template_name = 'test_app/test_page_1.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = TestModel.objects.all().order_by('name')
+        context['posts'] = posts
+        return context
 
