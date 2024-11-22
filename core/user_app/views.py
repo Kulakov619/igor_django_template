2c65d59f66758acebb712a3a8353417dafecb14d
Kulakov619 <kulakov_i_v@school619.ru>
2024-11-22 10:03:33 +0300

    Init 11

--- core/user_app/views.py
+++ core/user_app/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.shortcuts import render, reverse
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 class UserLoginView(LoginView):
     template_name = "user_app/login.html"
-    success_url = "/"
+
+    def get_success_url(self):
+        if self.request.user.is_superuser:  # Check if admin
+            return reverse('admin:index')
+        else:
+            return reverse('test_page')
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+
+
